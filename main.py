5cdfe7ff51ddc3b011846119591b5ef92644ebdd
UltraQbik <no1skill@yandex.ru>
2024-10-04 01:40:47 +0300

    Add initialization method

--- main.py
+++ main.py
@@ -15,7 +15,19 @@ class Application:
         self.width: int = os.get_terminal_size().columns // 2
         self.height: int = os.get_terminal_size().lines
 
-        self.board: np.ndarray = np.zeros([self.width, self.height], dtype=np.bool_)
+        self.board: np.ndarray | None = None
+
+        self.initialize()
+
+    def initialize(self):
+        """
+        Initialize the application
+        """
+
+        self.board = np.zeros([self.width, self.height], dtype=np.bool_)  # init board with 0's
+
+        os.system("cls" if os.name == "nt" else "clear")  # init ANSI escape codes and clear terminal
+        print("\033[?25l", end="", flush=True)  # make cursor invisible
 
     def run(self):
         """
