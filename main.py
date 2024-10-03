931568a9f2efba590f2941c2cb4a72b466e105ba
UltraQbik <no1skill@yandex.ru>
2024-10-04 01:36:07 +0300

    Boilerplate code

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+
+
+class Application:
+    """
+    Main application class
+    """
+
+    def __init__(self):
+        pass
+
+
+def main():
+    app = Application()
+    # app.run()
+
+
+if __name__ == '__main__':
+    main()
